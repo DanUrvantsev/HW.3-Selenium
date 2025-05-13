04000b521dc66645dc74b17cd14a65d7c162e338
DanUrvantsev <laorant8@gmail.com>
2025-05-13 21:29:21 +0700

    selenium test fix

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -5,19 +5,33 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrderTest {
     private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll(){
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeEach
     public void openBrowser() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
     @Test
     public void testCardOrder() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Иванов");
@@ -28,8 +42,4 @@ public class CardOrderTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message);
     }
 
-    @AfterEach
-    public void closeBrowser() {
-        driver.quit();
-    }
 }
